40ae3c69841193f18ecfe1049d9b526ad3840892
MauriBoye <mauriboye@gmail.com>
2021-05-15 17:32:40 -0300

    Ej.2-(Suma lenta)

---
+++ Archivos/tp4_ej2.py
@@ -0,0 +1,31 @@
+################
+# Mauricio Boyé - @MauriBoye
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+from tp4_ej1 import ingreso_numero
+
+def suma_lenta():
+    print("Suma lenta de dos numeros")
+    numero1=ingreso_numero("Ingrese el primer numero: ")
+    numero2=ingreso_numero("Ingrese el segundo numero: ")
+    contador=0
+    resultado=numero1
+    if numero2 > 0:
+        while contador < numero2 :
+            resultado += 1
+            contador +=1
+    else:
+        while contador > numero2 :
+            resultado -= 1
+            contador -=1
+    print(f"El resultado es: {resultado}")
+    
+
+def prueba():
+    suma_lenta()
+    pass
+
+if __name__ == "__main__":
+    prueba()
