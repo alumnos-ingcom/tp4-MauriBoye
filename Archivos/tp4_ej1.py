982c138ebefe6c72b41711ce69b0c3501be94778
MauriBoye <mauriboye@gmail.com>
2021-05-20 19:48:19 -0300

    Agregue los raise y cambie el método de entrada en las variables

--- Archivos/tp4_ej1.py
+++ Archivos/tp4_ej1.py
@@ -2,36 +2,71 @@
 # Mauricio Boyé - @MauriBoye
 # Plantilla de ejercicio
 # UNRN Andina - Introducción a la Ingenieria en Computación
+################################
+# Mauricio Boyé - @MauriBoye
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-def ingreso_numero(mensaje):
-    while True:
-        ingreso=input(mensaje)
-        try:
-            entero=int(ingreso)
-            if entero >= 0 or entero < 0:
-                return entero
-                break
-        except ValueError:
-            print(f"({ingreso}) no es un numero!")
-            print("")    
 
-def ingreso_numero_limite():
-    print("Ingrese los valores limites")
-    minimo=ingreso_numero("Minimo: ")
-    maximo=ingreso_numero("Maximo: ")
+"""
+Primer ejercicio
+"""
+
+class IngresoIncorrecto(Exception):
+    """Esta es la Excepcion para el ingreso incorrecto"""
+    pass
+
+def ingreso_numero(mensaje):
+    """
+    Esta funcion muestra un mensaje y agrega la # para indicar el ingreso
+    de un número entero.
+    """
+    ingreso=input(mensaje)
+    try:
+        entero=int(ingreso)
+        print("Todo bien master")
+    except ValueError as err:
+        raise IngresoIncorrecto(f"'{ingreso}' no es un número!") from err
+    return entero    
+
+def ingreso_numero_limite(mensaje,minimo=0,maximo=10):
+    print(mensaje)
+    print(f"Minimo: {minimo}")
+    print(f"Maximo: {maximo}")
     while True:
         entero=ingreso_numero("Ingrese un numero: ")
         if entero > minimo and entero < maximo:
-            print("Todo bien master")
             break
         else:
-            print(f"({entero}) no esta dentro del rango")
-            print("")
-            continue
+            raise IngresoIncorrecto(f"'{entero}' no esta dentro del rango!")
+        return entero
 
+def ingreso_numero_reintento(mensaje, cantidad_reintentos=5):
+    intentos= cantidad_reintentos
+    while intentos>0:
+        try:
+            return ingreso_numero(mensaje)
+        except IngresoIncorrecto:
+            intentos-=1
+            print(f"Valor incorrecto, le queda {intentos} intentos")
+            print("")
+    raise IngresoIncorrecto("Se acabaron los intentos disponibles")
+        
 def prueba():
-    ingreso_numero_limite()
-    pass
+    print ("-----------------------------------------------------------------------------------")
+    print ("ingreso_numero()")
+    print ("-----------------------------------------------------------------------------------")
+    ingreso_numero("Ingrese un numero: ")
+    print ("")
+    print ("-----------------------------------------------------------------------------------")
+    print ("ingreso_numero_limite()")
+    print ("-----------------------------------------------------------------------------------")
+    ingreso_numero_limite("Ingrese un numero entero entre los valores límites")
+    print ("")
+    print ("-----------------------------------------------------------------------------------")
+    print ("ingreso_numero_reintento()")
+    print ("-----------------------------------------------------------------------------------")
+    ingreso_numero_reintento("Ingrese un numero: ")
 
 if __name__ == "__main__":
     prueba()
