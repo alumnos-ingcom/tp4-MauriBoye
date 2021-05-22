e5ba43717d79b12f0360a83e4f9b523c8da18532
MauriBoye <mauriboye@gmail.com>
2021-05-22 13:54:33 -0300

    Puse los print en la función prueba()

--- Archivos/tp4ej3.py
+++ Archivos/tp4ej3.py
@@ -13,13 +13,10 @@ def convertir_a_fahrrenheit(centigrados):
     Esta funcion pide el ingreso de una temperatura en centigrados
     para indicar la conversion a fahrrenheit.
     """
-    print ("Conversion de centigrados a fahrrenheit")
     try:
-        centigrados = float(centigrados)
         fahrrenheit = (centigrados * 9/5) + 32
     except ValueError:
         raise tp4ej1.IngresoIncorrecto(f"'{centigrados}' no es un número!")
-    print (f"{centigrados}°C son {fahrrenheit}°F")
     return fahrrenheit
 
 def convertir_a_centigrados(fahrrenheit):
@@ -27,25 +24,23 @@ def convertir_a_centigrados(fahrrenheit):
     Esta funcion pide el ingreso de una temperatura en fahrrenheit
     para indicar la conversion a centigrados.
     """
-    print ("Conversion de fahrrenheit a centigrados")
     try:
-        fahrrenheit = float(fahrrenheit)
         centigrados = (fahrrenheit - 32) * 5/9
     except ValueError:
         raise tp4ej1.IngresoIncorrecto(f"'{fahrrenheit}' no es un número!")
-    print (f"{fahrrenheit}°F son {centigrados}°C")
     return centigrados
 
 def prueba():
-    print ("-----------------------------------------------------------------------------------")
-    print ("convertir_a_fahrrenheit()")
-    print ("-----------------------------------------------------------------------------------")
-    convertir_a_fahrrenheit(24)
-    print ("")
-    print ("-----------------------------------------------------------------------------------")
-    print ("convertir_a_centigrados()")
-    print ("-----------------------------------------------------------------------------------")
-    convertir_a_centigrados(75.2)
+    tp4ej1.marco("convertir_a_fahrrenheit()")
+    print ("Conversion de centigrados a fahrrenheit")
+    centigrados = 24
+    fahrrenheit = convertir_a_fahrrenheit(centigrados)
+    print (f"{centigrados}°C son {fahrrenheit}°F")
+    tp4ej1.marco("convertir_a_centigrados()")
+    print ("Conversion de fahrrenheit a centigrados")
+    fahrrenheit = 75.2
+    centigrados = convertir_a_centigrados(fahrrenheit)
+    print (f"{fahrrenheit}°F son {centigrados}°C")
 
 if __name__ == "__main__":
     prueba()
