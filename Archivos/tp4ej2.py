235684c3f2aaef3224fca8bd0e23c87b3ebbded7
MauriBoye <mauriboye@gmail.com>
2021-05-23 12:25:42 -0300

    Elimine el manejo de excepciones en la función suma_lenta() y utilice la función ingreso_numero() para pedir los números con un mensaje

--- Archivos/tp4ej2.py
+++ Archivos/tp4ej2.py
@@ -13,26 +13,23 @@ def suma_lenta(numero1,numero2):
     Esta funcion pide el ingreso de 2 numeros para indicar la
     suma entre ellos sin hacer la operacion de manera directa.
     """
-    try:
-        contador=0
-        resultado=numero1
-        if numero2 > 0:
-            while contador < numero2 :
-                resultado = resultado + 1
-                contador = contador + 1
-        else:
-            while contador > numero2 :
-                resultado = resultado - 1
-                contador = contador - 1
-    except ValueError:
-        raise tp4ej1.IngresoIncorrecto(f"Eso no es un número!")
+    contador=0
+    resultado=numero1
+    if numero2 > 0:
+        while contador < numero2 :
+            resultado = resultado + 1
+            contador = contador + 1
+    else:
+        while contador > numero2 :
+            resultado = resultado - 1
+            contador = contador - 1
     return resultado
 
 def prueba():
     tp4ej1.marco("suma_lenta()")
     print("Suma lenta de dos numeros")
-    numero1=3
-    numero2=-5
+    numero1=tp4ej1.ingreso_numero("Ingrese el 1er numero: ")
+    numero2=tp4ej1.ingreso_numero("Ingrese el 2do numero: ")
     resultado=suma_lenta(numero1,numero2)
     print(f"({numero1})+({numero2})= {resultado}")
 
