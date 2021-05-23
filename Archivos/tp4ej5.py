b667fca5c04f5ca0d93b63db7bf8a9e4c52ca26a
MauriBoye <mauriboye@gmail.com>
2021-05-23 12:51:26 -0300

    Cambia los print de la funcion signo() por un return que indica su signo y agrega la función ingreso_numero() para pedir los números con un mensaje

--- Archivos/tp4ej5.py
+++ Archivos/tp4ej5.py
@@ -13,16 +13,23 @@ def signo(numero):
     Esta funcion pide el ingreso de un numero e indica si es par o inpar
     """
     if numero==0:
-        print(f"{numero} es cero")
+        return 0
     elif numero!=0 and numero%2==0:
-        print(f"{numero} es par")
+        return 2
     elif numero!=0:
-        print(f"{numero} es inpar")
+        return 1
 
 def prueba():
     tp4ej1.marco("signo()")
     print ("Par, inpar o cero")
-    signo(21)
+    numero = tp4ej1.ingreso_numero("Ingrese un numero: ")
+    valor = signo(numero)
+    if valor == 0:
+        print(f"{numero} es cero")
+    elif valor == 1:
+        print(f"{numero} es inpar")
+    elif valor == 2:
+        print(f"{numero} es par")
     
 if __name__ == "__main__":
     prueba()
