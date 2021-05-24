3bd0189487ec4d147ca978f5c8fbbdcf19faa1da
MauriBoye <mauriboye@gmail.com>
2021-05-24 13:28:59 -0300

    Corregí la función de par o impar a positivo o negativo

--- Archivos/tp4ej5.py
+++ Archivos/tp4ej5.py
@@ -10,26 +10,26 @@ import tp4ej1
 
 def signo(numero):
     """
-    Esta funcion pide el ingreso de un numero e indica si es par o inpar
+    Esta funcion pide el ingreso de un numero para indicar su signo
     """
     if numero==0:
         return 0
-    elif numero!=0 and numero%2==0:
-        return 2
-    elif numero!=0:
+    elif numero<0:
+        return -1
+    elif numero>0:
         return 1
 
 def prueba():
     tp4ej1.marco("signo()")
-    print ("Par, inpar o cero")
+    print ("Positivo, negativo o cero")
     numero = tp4ej1.ingreso_numero("Ingrese un numero: ")
     valor = signo(numero)
     if valor == 0:
         print(f"{numero} es cero")
+    elif valor == -1:
+        print(f"{numero} es negativo")
     elif valor == 1:
-        print(f"{numero} es inpar")
-    elif valor == 2:
-        print(f"{numero} es par")
-    
+        print(f"{numero} es positivo")
+
 if __name__ == "__main__":
     prueba()
