ee02134df1d9731bcf4e9b3409e831b5bdfbbdb3
MauriBoye <mauriboye@gmail.com>
2021-05-24 13:56:24 -0300

    Cambio de nombre en las variables lista y nueva_lista a lista_aleatoria y nueva_lista_aleatoria

--- Archivos/tp4ej6.py
+++ Archivos/tp4ej6.py
@@ -9,45 +9,45 @@ Sexto ejercicio
 import tp4ej1
 import random
 
-def lista(cantidad_numeros):
-    nueva_lista=[]
+def lista_aleatoria(cantidad_numeros):
+    nueva_lista_aleatoria=[]
     for i in range(cantidad_numeros):
-        nueva_lista.append(random.randint(-100,100))
-    return nueva_lista
+        nueva_lista_aleatoria.append(random.randint(-100,100))
+    return nueva_lista_aleatoria
 
-def minimo(nueva_lista):
+def minimo(nueva_lista_aleatoria):
     """
     Esta función devuelve el menor numero de una lista
     """
     i=0
-    menor=nueva_lista[i]
-    while i < len(nueva_lista):
-        if menor > nueva_lista[i]:
-            menor=nueva_lista[i]
+    menor=nueva_lista_aleatoria[i]
+    while i < len(nueva_lista_aleatoria):
+        if menor > nueva_lista_aleatoria[i]:
+            menor=nueva_lista_aleatoria[i]
         i = i+1
     return menor
 
-def maximo(nueva_lista):
+def maximo(nueva_lista_aleatoria):
     """
     Esta función devuelve el mayor numero de una lista
     """
     i=0
-    mayor=nueva_lista[i]
-    while i < len(nueva_lista):
-        if mayor < nueva_lista[i]:
-            mayor=nueva_lista[i]
+    mayor=nueva_lista_aleatoria[i]
+    while i < len(nueva_lista_aleatoria):
+        if mayor < nueva_lista_aleatoria[i]:
+            mayor=nueva_lista_aleatoria[i]
         i = i+1
     return mayor
 
 def prueba():
-    nueva_lista = lista(tp4ej1.ingreso_numero("Ingrese la cantidad de numeros para la lista: "))
+    nueva_lista_aleatoria = lista_aleatoria(tp4ej1.ingreso_numero("Ingrese la cantidad de numeros para la lista: "))
     tp4ej1.marco("minimo()")
-    menor = minimo(nueva_lista)
-    print(nueva_lista)
+    menor = minimo(nueva_lista_aleatoria)
+    print(nueva_lista_aleatoria)
     print(f"El menor numero de la lista es {menor}")
     tp4ej1.marco("maximo()")
-    mayor = maximo(nueva_lista)
-    print(nueva_lista)
+    mayor = maximo(nueva_lista_aleatoria)
+    print(nueva_lista_aleatoria)
     print(f"El mayor numero de la lista es {mayor}")
 
 if __name__ == "__main__":
