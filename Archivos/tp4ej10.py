26e004913e4484d5f838a8a8f80614165fc12b31
MauriBoye <mauriboye@gmail.com>
2021-05-27 10:25:31 -0300

    Corregí el retorno de la función por una tupla

--- Archivos/tp4ej10.py
+++ Archivos/tp4ej10.py
@@ -14,15 +14,13 @@ def factores_primos(numero):
     Esta funcion pide el ingreso de un numero y devuelve sus factores primos
     """
     i=1
-    tupla_factor_primo=()
     factor_primo=[1]
     while i <= numero:
         if tp4ej9.es_primo(i):
             if numero%i==0:
                 factor_primo.append(i)
         i=i+1
-    tupla_factor_primo = (factor_primo)
-    return tupla_factor_primo
+    return tuple(factor_primo)
 
 def prueba():
     tp4ej1.marco("factores_primos()")
